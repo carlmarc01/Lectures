b193f309615568ec4e9259461e095e365b334180
Carl Marc <148695036+carlmarc01@users.noreply.github.com>
2023-11-13 22:40:13 +0800

    Add files via upload

---
+++ MultiplicationTable10x10.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class MultiplicationTable10x10 {
+   public static void main(String[] args) {
+   for (int i =1; i <= 10; i++){
+        for (int b=1; b <=10; b++){
+            System.out.print(i*b + " ");
+        }
+        System.out.println();
+    }
+   }
+}
